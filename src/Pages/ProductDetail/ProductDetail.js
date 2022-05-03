ad52f67419a6b4bcf2bd55666176163f2a8ac6ba
Armin Sultana <arminsultana556@gmail.com>
2022-05-04 00:18:45 +0600

    Set dynamic id param in mongodb

--- src/Pages/ProductDetail/ProductDetail.js
+++ src/Pages/ProductDetail/ProductDetail.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
     const { inventoryId } = useParams()
+    const [product, setProduct]= useState({});
+    useEffect(()=>{
+        const url = `http://localhost:5000/product/${inventoryId}`;
+        fetch(url)
+        .then(res=>res.json())
+        .then(data =>setProduct(data))
+    },[])
     const navigate = useNavigate()
     return (
         <div>
-            <h2>Wlcome Product Details: {inventoryId}</h2>
+            <h2>Wlcome Product Details: {product.name}</h2>
             <div className="">
                 <button onClick={() => navigate("/manage")} class="btn btn-secondary bg-pink-500 px-20 mt-5 ml-2">EXPLORE MORE</button>
             </div>
